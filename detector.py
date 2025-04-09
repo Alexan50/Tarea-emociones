1bab3dcc4b012741c5f007f51f4118e7904fcaff
alexan50 <1557071@senati.pe>
2025-04-09 09:56:15 -0500

    comentario 4

--- detector.py
+++ detector.py
@@ -2,18 +2,14 @@ import cv2
 import mediapipe as mp
 import math
 
-
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)  
 cap.set(4, 720)   
 
-
 mpDibujo = mp.solutions.drawing_utils
 ConfDibu = mpDibujo.DrawingSpec(thickness=1, circle_radius=1)  
 
-#la malla facial
 mpMallaFacial = mp.solutions.face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True) 
-
 
 lista = []
 
@@ -23,13 +19,13 @@ while True:
         break  
     
     frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
     
     resultados = mpMallaFacial.process(frameRGB)
 
-    if resultados.multi_face_landmarks:  # Si detectamos algún rostro
+    emocion_detectada = None
+
+    if resultados.multi_face_landmarks:  
         for rostros in resultados.multi_face_landmarks:
-            
             mpDibujo.draw_landmarks(frame, rostros, mp.solutions.face_mesh.FACEMESH_TESSELATION, ConfDibu, ConfDibu)
 
             lista.clear()
@@ -39,42 +35,42 @@ while True:
                 x, y = int(puntos.x * an), int(puntos.y * al)
                 lista.append([id, x, y])
 
-                if len(lista) == 468:  # Aseguramos que se recojan todos los puntos
-                  
+                if len(lista) == 468:  
                     x1, y1 = lista[65][1:]
                     x2, y2 = lista[158][1:]
                     longitud1 = math.hypot(x2 - x1, y2 - y1)
 
-                    
                     x3, y3 = lista[295][1:]
                     x4, y4 = lista[385][1:]
                     longitud2 = math.hypot(x4 - x3, y4 - y3)
-
 
                     x5, y5 = lista[78][1:]
                     x6, y6 = lista[308][1:]
                     longitud3 = math.hypot(x6 - x5, y6 - y5)
 
-                    
                     x7, y7 = lista[13][1:]
                     x8, y8 = lista[14][1:]
                     longitud4 = math.hypot(x8 - x7, y8 - y7)
 
-                 
-                    if longitud3 > 70:  
-                        cv2.putText(frame, 'Persona Feliz', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 3)
-
-
-                    
                     if longitud4 > 50:  
-                        cv2.putText(frame, 'Persona Asombrada', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+                        emocion_detectada = 'Persona Asombrada'
+                    
+                    elif longitud1 < 30 and longitud2 < 19 and longitud3 > 80 and longitud3 < 95 and longitud4 < 5:  
+                        emocion_detectada = 'Persona Enojada'
+                    
+                    elif longitud1 > 10 and longitud1 < 30 and longitud2 > 20 and longitud2 < 30 and longitud3 > 80 and longitud3 < 109 and longitud4 < 5:  
+                        emocion_detectada = 'Persona Triste'
+                    
+                    elif longitud3 > 70:  
+                        emocion_detectada = 'Persona Feliz'
 
                     
-                    elif longitud1 < 30 and longitud2 < 19 and longitud3 > 80 and longitud3 < 95 and longitud4 < 5:
-                        cv2.putText(frame, 'Persona Enojada', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-
-                    elif longitud1 > 10 and longitud1 < 30 and longitud2 > 20 and longitud2 < 30 and longitud3 > 80 and longitud3 < 109 and longitud4 < 5:
-                        cv2.putText(frame, 'Persona Triste', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+                    if emocion_detectada:
+                        cv2.putText(frame, emocion_detectada, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                                   (0, 255, 0) if emocion_detectada == 'Persona Asombrada' else 
+                                   (0, 0, 255) if emocion_detectada == 'Persona Enojada' else 
+                                   (255, 0, 0) if emocion_detectada == 'Persona Triste' else 
+                                   (0, 255, 255), 3)
 
     cv2.imshow("Reconocimiento de Emociones", frame)
     t = cv2.waitKey(1)
